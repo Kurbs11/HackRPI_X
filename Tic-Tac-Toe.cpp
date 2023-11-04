9af24612218d2b7483e178479161ff4a8c304e1a
ChenHY1217 <haoyuan2004@gmail.com>
2023-11-04 16:09:48 -0400

    Update Tic-Tac-Toe.cpp

--- Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe.cpp
@@ -1,25 +1,13 @@
-#include <iostream>
+
+def output(arr):
+
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            print(arr[i][j], "|")
 
 
-int main(){
+rows, cols = (3, 3)
+arr = [["."]*cols]*rowss
+output(arr)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
-}
