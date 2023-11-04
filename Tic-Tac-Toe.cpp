1fa5b4c0a00a3c55a8e2408318191c1acec6eec8
ChenHY1217 <haoyuan2004@gmail.com>
2023-11-04 16:43:27 -0400

    update print

--- Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe.cpp
@@ -5,20 +5,6 @@ using namespace std;
 void print(const vector<vector<char> >& board){
 
     for(unsigned int i = 0; i < board.size(); i++){
-        for(unsigned int k = 0; k < board.size(); k++){
-            cout << "   ";
-            if(k != board.size()-1){
-                cout << "|";
-            }
-        }
-        for(unsigned int j = 0; j < board[0].size(); j++){
-            
-            cout << " " << board[i][j] << " ";
-            if(j != board[0].size() - 1){
-                cout << "|"
-            }
-            
-        }
         
         for(unsigned int k = 0; k < board.size(); k++){
             cout << "   ";
@@ -26,6 +12,28 @@ void print(const vector<vector<char> >& board){
                 cout << "|";
             }
         }
+        cout << "\n";
+        for(unsigned int j = 0; j < board[0].size(); j++){
+            
+            cout << " " << board[i][j] << " ";
+            if(j != board[0].size() - 1){
+                cout << "|";
+            }
+            
+        }
+        cout << "\n";
+        
+        for(unsigned int k = 0; k < board.size(); k++){
+            cout << "   ";
+            if(k != board.size()-1){
+                cout << "|";
+            }
+        }
+        cout << "\n";
+
+        cout << string((board.size()*board[0].size()) + (board.size() - 1), '-') << "\n";
+
+
     }
 
 
@@ -34,9 +42,10 @@ void print(const vector<vector<char> >& board){
 
 int main(){
 
-    int row, col = 3;
-    vector<vector<char> > board(row, vector<char>(col, '0'));
-    int space_left = row*col;
+    int rows = 3;
+    int cols = 3;
+    vector<vector<char> > board(rows, vector<char>(cols, '0'));
+    int space_left = rows*cols;
 
     print(board);
 
