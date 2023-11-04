cce1dc697806b68da0721480e898912a9e72e9c9
ChenHY1217 <haoyuan2004@gmail.com>
2023-11-04 17:19:35 -0400

    print2 implemented

--- Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe.cpp
@@ -32,24 +32,40 @@ void print(const vector<vector<char> >& board){
         cout << "\n";
 
         if(i != board.size() - 1){
-            cout << string((board.size()*board[0].size()) + (board.size() - 1), '-') << "\n";
+            cout << string((3*board[0].size()) + (board.size() - 1), '-') << "\n";
         }
+    }
+}
 
+void print2(const vector<vector<char> >& board){
+
+    for(unsigned int i = 0; i < board.size(); i++){
+        for(unsigned int j = 0; j < board[0].size(); j++){
+            
+            cout << " " << board[i][j] << " ";
+            if(j != board[0].size() - 1){
+                cout << "|";
+            }
+            
+        }
+        cout << "\n";
+        
+        if(i != board.size() - 1){
+            cout << string((3*board[0].size()) + (board.size() - 1), '-') << "\n";
+        }
 
     }
 
-
 }
-
 
 int main(){
 
-    int rows = 3;
-    int cols = 3;
+    int rows = 5;
+    int cols = 5;
     vector<vector<char> > board(rows, vector<char>(cols, 'X'));
     int space_left = rows*cols;
 
-    print(board);
+    print2(board);
 
 
     
